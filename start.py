5c1518895cf49c4e0334fa16e5e9a21e5054875a
Floozutter <me@floozutter.site>
2021-11-25 13:36:50 -0800

    start program structure

--- start.py
+++ start.py
@@ -1,3 +1,15 @@
 """
 starts an Advent of Code 2021 day by copying from the day00/ template and requesting the input
 """
+
+import argparse
+from typing import Optional
+
+def parse_args() -> tuple[str, Optional[str]]:
+    raise NotImplementedError
+
+def main(daystr: str, cookie: Optional[str] = None) -> None:
+    raise NotImplementedError
+
+if __name__ == "__main__":
+    main(*parse_args())
