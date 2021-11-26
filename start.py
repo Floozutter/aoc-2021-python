f73ef211db3b76b08278eaa8dda44789584b5d39
Floozutter <me@floozutter.site>
2021-11-26 13:16:26 -0800

    implement parse_args

--- start.py
+++ start.py
@@ -2,11 +2,25 @@
 starts an Advent of Code 2021 day by copying from the day00/ template and requesting the input
 """
 
-import argparse
+from argparse import ArgumentParser
+from pathlib import Path
 from typing import Optional
 
 def parse_args() -> tuple[str, Optional[str]]:
-    raise NotImplementedError
+    parser = ArgumentParser(description = __doc__)
+    parser.add_argument("day", type = Path, metavar = "DAY",
+        help = "path of directory to create by copying day00/"
+    )
+    cookie_group = parser.add_mutually_exclusive_group()
+    cookie_group.add_argument("--raw", type = str, metavar = "COOKIE",
+        help = "Advent of Code session cookie to request DAY's input with"
+    )
+    cookie_group.add_argument("--file", type = Path, metavar = "COOKIE",
+        help = "path to file containing Advent of Code session cookie"
+    )
+    args = parser.parse_args()
+    cookie = args.file.read_bytes() if args.file is not None else args.raw
+    return args.day, cookie
 
 def main(daystr: str, cookie: Optional[str] = None) -> None:
     raise NotImplementedError
