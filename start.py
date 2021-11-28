8f949d7b40d64379802f5a03a69a0b3d21ca9654
Floozutter <me@floozutter.site>
2021-11-28 14:53:41 -0800

    add skeleton to main
    doot

--- start.py
+++ start.py
@@ -31,8 +31,32 @@ def parse_args() -> tuple[Path, Path, Optional[str]]:
     cookie = args.file.read_bytes() if args.file is not None else args.raw
     return args.template, args.day, cookie
 
-def main(template: Path, day: Path, cookie: Optional[str] = None) -> None:
+def parse_day_number(day: Path) -> Optional[int]:
     raise NotImplementedError
+
+def main(template: Path, day: Path, cookie: Optional[str] = None) -> None:
+    # copy the directory tree from template to day
+    print(f"copying from {template}/ to {day}/...", end = " ")
+    raise NotImplementedError
+    print("done.")
+    # exit if no cookie to request with
+    if cookie is None:
+        return
+    # parse day number from day path
+    print(f"parsing day number from {day}/...", end = " ")
+    day_number = parse_day_number(str(day))
+    if day_number is None:
+        print(f"error: could not parse day number from {day}!")
+        return
+    print(f"parsed as day {day_number}.")
+    # request input for the parsed day number using cookie
+    print(f"requesting input for day {day_number} using cookie...", end = " ")
+    raise NotImplementedError
+    print("done.")
+    # write input to file in day path
+    print(f"writing input to {...}", end = " ")
+    raise NotImplementedError
+    print("done.")
 
 if __name__ == "__main__":
     main(*parse_args())
