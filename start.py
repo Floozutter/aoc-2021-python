405a739892c759e95046d5327e6447b3911a867c
Floozutter <me@floozutter.site>
2021-11-29 13:51:25 -0800

    get inputfile as argument

--- start.py
+++ start.py
@@ -7,16 +7,24 @@ from pathlib import Path
 from typing import Optional
 
 DEFAULT_TEMPLATE = Path("day00/")
+DEFAULT_INPUTFILE = Path("input.txt")
 
-def parse_args() -> tuple[Path, Path, Optional[str]]:
+def parse_day_number(day: Path) -> Optional[int]:
+    raise NotImplementedError
+
+def parse_args() -> tuple[Path, Path, Path, Optional[str]]:
     parser = ArgumentParser(description = __doc__)
     parser.add_argument("day", type = Path,
         metavar = "DAY",
-        help = "path of directory to create by copying from the template"
+        help = "path of directory to create by copying from the template",
     )
     parser.add_argument("--template", type = Path, default = DEFAULT_TEMPLATE,
         metavar = "TEMPLATE",
         help = f"path of template for DAY, defaults to {DEFAULT_TEMPLATE}/",
+    )
+    parser.add_argument("--inputfile", type = Path, default = DEFAULT_INPUTFILE,
+        metavar = "INPUTFILE",
+        help = f"path relative from TEMPLATE to write the requested input to",
     )
     cookie_group = parser.add_mutually_exclusive_group()
     cookie_group.add_argument("--raw", type = str,
@@ -28,20 +36,33 @@ def parse_args() -> tuple[Path, Path, Optional[str]]:
         help = "path to file containing Advent of Code session cookie",
     )
     args = parser.parse_args()
+    joined_inputfile = args.template / args.inputfile
     cookie = args.file.read_bytes() if args.file is not None else args.raw
-    return args.template, args.day, cookie
+    return (
+        args.day,
+        args.template,
+        joined_inputfile,
+        cookie,
+    )
 
-def parse_day_number(day: Path) -> Optional[int]:
-    raise NotImplementedError
-
-def main(template: Path, day: Path, cookie: Optional[str] = None) -> None:
+def main(
+    day: Path,
+    template: Path,
+    inputfile: Path,
+    cookie: Optional[str] = None
+) -> None:
     # copy the directory tree from template to day
     print(f"copying from {template}/ to {day}/...", end = " ")
     raise NotImplementedError
     print("done.")
-    # exit if no cookie to request with
+    # write blank inputfile and exit if no cookie provided
     if cookie is None:
+        print("no session cookie provided to request input with!")
+        print(f"writing blank {inputfile}...", end = " ")
+        raise NotImplementedError
+        print("done.")
         return
+    print("session cookie provided to request input.")
     # parse day number from day path
     print(f"parsing day number from {day}/...", end = " ")
     day_number = parse_day_number(str(day))
@@ -53,8 +74,8 @@ def main(template: Path, day: Path, cookie: Optional[str] = None) -> None:
     print(f"requesting input for day {day_number} using cookie...", end = " ")
     raise NotImplementedError
     print("done.")
-    # write input to file in day path
-    print(f"writing input to {...}", end = " ")
+    # write input to inputfile
+    print(f"writing input to {inputfile}...", end = " ")
     raise NotImplementedError
     print("done.")
 
