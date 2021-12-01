3e90656ed34a339dd40d39c23d1787995951146a
Floozutter <me@floozutter.site>
2021-11-30 22:11:04 -0800

    clean day01 again
    playing around with different implementations for this day is pretty
    fun! i'm too lazy to improve the moving sum performance though

--- day01/main.py
+++ day01/main.py
@@ -4,12 +4,9 @@ with open(INPUTPATH) as ifile:
     raw = ifile.read()
 depths = tuple(map(int, raw.strip().split()))
 
-print(sum(
-    1 for last, curr in zip(depths, depths[1:])
-    if last < curr
-))
+number_of_increases = lambda seq: sum(1 for l, r in zip(seq, seq[1:]) if l < r)
 
-print(sum(
-    1 for a, b, c, d in zip(*map(lambda i: depths[i:], range(4)))
-    if a+b+c < b+c+d
-))
+print(number_of_increases(depths))
+print(number_of_increases(tuple(
+    sum(window) for window in zip(depths, depths[1:], depths[2:])
+)))
