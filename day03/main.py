8354fa675329158fd1aeada7b72f71412b599650
Floozutter <me@floozutter.site>
2021-12-03 16:35:17 -0800

    clean day03 part 2

--- day03/main.py
+++ day03/main.py
@@ -3,37 +3,21 @@ INPUTPATH = "input.txt"
 with open(INPUTPATH) as ifile:
     raw = ifile.read()
 report = tuple(raw.strip().split())
-columns = tuple(map(tuple, zip(*report)))
 
+columns = tuple(map(tuple, zip(*report)))
 from statistics import mode
 gamma_bits = "".join(mode(bits) for bits in columns)
 epsilon_bits = "".join("0" if b == "1" else "1" for b in gamma_bits)
 print(int(gamma_bits, 2) * int(epsilon_bits, 2))
 
-numbers = set(report)
-i = 0
-while len(numbers) > 1:
-    zeros = 0
-    for n in numbers:
-        if n[i] == "0":
-            zeros += 1
-    ones = len(numbers) - zeros
-    for n in tuple(numbers):
-        if n[i] == ("0" if ones >= zeros else "1"):
-            numbers.remove(n)
-    i += 1
-o = int(next(iter(numbers)), 2)
-numbers = set(report)
-i = 0
-while len(numbers) > 1:
-    zeros = 0
-    for n in numbers:
-        if n[i] == "0":
-            zeros += 1
-    ones = len(numbers) - zeros
-    for n in tuple(numbers):
-        if n[i] == ("0" if ones < zeros else "1"):
-            numbers.remove(n)
-    i += 1
-c = int(next(iter(numbers)), 2)
-print(o*c)
+from typing import Sequence, Callable
+def find_rating(bitstrings: Sequence[str], criteria: Callable[[str], str], index: int = 0) -> int:
+    if len(bitstrings) <= 1:
+        return int(bitstrings[0], 2)
+    else:
+        keep = criteria("".join(s[index] for s in bitstrings))
+        return find_rating(tuple(s for s in bitstrings if s[index] == keep), criteria, index + 1)
+ceildiv = lambda n, d: -(n // -d)
+oxy = find_rating(report, lambda col: "01"[col.count("1") >= ceildiv(len(col), 2)])
+co2 = find_rating(report, lambda col: "01"[col.count("1")  < ceildiv(len(col), 2)])
+print(oxy * co2)
