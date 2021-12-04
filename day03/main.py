afb6f1bf81f896fc553b1ab6f2b392b82b3a231e
Floozutter <me@floozutter.site>
2021-12-03 16:59:34 -0800

    reorder find_rating params for easier partial app

--- day03/main.py
+++ day03/main.py
@@ -11,13 +11,13 @@ epsilon_bits = "".join("0" if b == "1" else "1" for b in gamma_bits)
 print(int(gamma_bits, 2) * int(epsilon_bits, 2))
 
 from typing import Sequence, Callable
-def find_rating(bitstrings: Sequence[str], criteria: Callable[[str], str], index: int = 0) -> int:
+def find_rating(criteria: Callable[[str], str], bitstrings: Sequence[str], index: int = 0) -> int:
     if len(bitstrings) <= 1:
         return int(bitstrings[0], 2)
     else:
         keep = criteria("".join(s[index] for s in bitstrings))
-        return find_rating(tuple(s for s in bitstrings if s[index] == keep), criteria, index + 1)
+        return find_rating(criteria, tuple(s for s in bitstrings if s[index] == keep), index + 1)
 ceildiv = lambda n, d: -(n // -d)
-oxy = find_rating(report, lambda col: "01"[col.count("1") >= ceildiv(len(col), 2)])
-co2 = find_rating(report, lambda col: "01"[col.count("1")  < ceildiv(len(col), 2)])
+oxy = find_rating(lambda col: "01"[col.count("1") >= ceildiv(len(col), 2)], report)
+co2 = find_rating(lambda col: "01"[col.count("1")  < ceildiv(len(col), 2)], report)
 print(oxy * co2)
