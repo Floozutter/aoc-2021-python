5e19069606aa4e5f6943660fabb986b6ecef58e6
Floozutter <me@floozutter.site>
2021-12-08 22:00:45 -0800

    clean day09

--- day09/main.py
+++ day09/main.py
@@ -2,28 +2,24 @@ INPUTPATH = "input.txt"
 #INPUTPATH = "input-test.txt"
 with open(INPUTPATH) as ifile:
     raw = ifile.read()
-heightmap = tuple(tuple(map(int, row)) for row in raw.strip().split())
+hmap = tuple(tuple(map(int, row)) for row in raw.strip().split())
 adjacents = lambda i, j: (
-    (i+di, j+dj)
-    for di, dj in ((-1, 0), (1, 0), (0, -1), (0, 1))
-    if 0 <= i+di < len(heightmap) and 0 <= j+dj < len(heightmap[0])
+    (r, c) for r, c in ((i-1, j), (i+1, j), (i, j-1), (i, j+1))
+    if 0 <= r < len(hmap) and 0 <= c < len(hmap[0])
 )
-lows = []
-for i, row in enumerate(heightmap):
-    for j, h in enumerate(row):
-        if all(
-            heightmap[i][j] < heightmap[r][c]
-            for r, c in adjacents(i, j)
-        ):
-            lows.append((i, j))
-print(sum(heightmap[i][j] for i, j in lows) + len(lows))
 
-def basin(i, j, visited) -> int:
-    size = 1
-    for r, c in adjacents(i, j):
-        if (r, c) not in visited and heightmap[r][c] > heightmap[i][j] and heightmap[r][c] != 9:
-            visited.add((r, c))
-            size += basin(r, c, visited)
-    return size
-sizes = sorted(basin(i, j, set()) for i, j in lows)
-print(sizes[-1]*sizes[-2]*sizes[-3])
+lows = tuple(
+    (i, j) for i, row in enumerate(hmap) for j, h in enumerate(row)
+    if all(h < hmap[r][c] for r, c in adjacents(i, j))
+)
+print(sum(hmap[i][j] + 1 for i, j in lows))
+
+from functools import reduce
+def basin(i: int, j: int) -> frozenset[tuple[int, int]]:
+    return reduce(
+        lambda a, b: a | b,
+        (basin(r, c) for r, c in adjacents(i, j) if hmap[r][c] > hmap[i][j] and hmap[r][c] != 9),
+        frozenset(((i, j),))
+    )
+sizes = tuple(len(basin(*l)) for l in lows)
+print(reduce(lambda a, b: a * b, sorted(sizes, reverse = True)[:3]))
