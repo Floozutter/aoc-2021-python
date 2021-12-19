dfd4feaa6ea0ae52fa664dc285411fd885cf4df5
Floozutter <me@floozutter.site>
2021-12-19 01:22:37 -0800

    add day19 part 1

--- day19/main.py
+++ day19/main.py
@@ -1,53 +1,80 @@
 INPUTPATH = "input.txt"
-INPUTPATH = "input-test.txt"
+#INPUTPATH = "input-test.txt"
 with open(INPUTPATH) as ifile:
     raw = ifile.read()
-reports = tuple(
-    tuple(tuple(map(int, line.split(","))) for line in report.strip().split("\n")[1:])
-    for report in raw.split("\n\n")
+
+from itertools import chain
+from collections import Counter, defaultdict
+from dataclasses import dataclass
+from typing import NamedTuple, Sequence, Callable
+
+@dataclass(frozen = True)
+class Point:
+    x: int
+    y: int
+    z: int
+    def __add__(self: "Point", other: "Point") -> "Point":
+        return Point(self.x + other.x, self.y + other.y, self.z + other.z)
+    def __sub__(self: "Point", other: "Point") -> "Point":
+        return Point(self.x - other.x, self.y - other.y, self.z - other.z)
+
+rotations: Sequence[Callable[[Point], Point]] = (
+    lambda p: Point(+p.x, +p.y, +p.z),
+    lambda p: Point(+p.x, -p.y, -p.z),
+    lambda p: Point(+p.x, +p.z, -p.y),
+    lambda p: Point(+p.x, -p.z, +p.y),
+    lambda p: Point(-p.x, +p.y, -p.z),
+    lambda p: Point(-p.x, -p.y, +p.z),
+    lambda p: Point(-p.x, +p.z, +p.y),
+    lambda p: Point(-p.x, -p.z, -p.y),
+    lambda p: Point(+p.y, +p.z, +p.x),
+    lambda p: Point(+p.y, -p.z, -p.x),
+    lambda p: Point(+p.y, +p.x, -p.z),
+    lambda p: Point(+p.y, -p.x, +p.z),
+    lambda p: Point(-p.y, +p.z, -p.x),
+    lambda p: Point(-p.y, -p.z, +p.x),
+    lambda p: Point(-p.y, +p.x, +p.z),
+    lambda p: Point(-p.y, -p.x, -p.z),
+    lambda p: Point(+p.z, +p.x, +p.y),
+    lambda p: Point(+p.z, -p.x, -p.y),
+    lambda p: Point(+p.z, +p.y, -p.x),
+    lambda p: Point(+p.z, -p.y, +p.x),
+    lambda p: Point(-p.z, +p.x, -p.y),
+    lambda p: Point(-p.z, -p.x, +p.y),
+    lambda p: Point(-p.z, +p.y, +p.x),
+    lambda p: Point(-p.z, -p.y, -p.x),
 )
 
-rotations = (
-    lambda x, y, z: (+x, +y, +z),
-    lambda x, y, z: (+x, -y, -z),
-    lambda x, y, z: (+x, +z, -y),
-    lambda x, y, z: (+x, -z, +y),
-    lambda x, y, z: (-x, +y, -z),
-    lambda x, y, z: (-x, -y, +z),
-    lambda x, y, z: (-x, +z, +y),
-    lambda x, y, z: (-x, -z, -y),
-    lambda x, y, z: (+y, +z, +x),
-    lambda x, y, z: (+y, -z, -x),
-    lambda x, y, z: (+y, +x, -z),
-    lambda x, y, z: (+y, -x, +z),
-    lambda x, y, z: (-y, +z, -x),
-    lambda x, y, z: (-y, -z, +x),
-    lambda x, y, z: (-y, +x, +z),
-    lambda x, y, z: (-y, -x, -z),
-    lambda x, y, z: (+z, +x, +y),
-    lambda x, y, z: (+z, -x, -y),
-    lambda x, y, z: (+z, +y, -x),
-    lambda x, y, z: (+z, -y, +x),
-    lambda x, y, z: (-z, +x, -y),
-    lambda x, y, z: (-z, -x, +y),
-    lambda x, y, z: (-z, +y, +x),
-    lambda x, y, z: (-z, -y, -x),
-)
-
-Point = tuple[int, int, int]
-
-from collections import Counter
-from operator import add, sub
-def sync_reports(rep_a: tuple[Point, ...], rep_b: tuple[Point, ...]) -> frozenset[Point] | None:
-    syncs = Counter()
+def syncf(rep_a: Sequence[Point], rep_b: Sequence[Point]) -> Callable[[Point], Point] | None:
+    syncs: Counter[tuple[int, Point]] = Counter()
     for i, r in enumerate(rotations):
         for a in rep_a:
             for b in rep_b:
-                d = tuple(map(sub, a, r(*b)))
-                syncs[i, d] += 1
-    i, d = max(syncs, key = syncs.get)
-    print(i, d, syncs[i, d])
-    if syncs[i, d] < 12:
-        return None
-    else:
-        return frozenset(rep_a) | frozenset(tuple(map(add, rotations[i](*b), d)) for b in rep_b)
+                syncs[i, a - r(b)] += 1
+    (i, d), count = max(syncs.items(), key = lambda pair: pair[1])
+    return None if count < 12 else lambda p: rotations[i](p) + d
+
+def synchronize(reports: Sequence[Sequence[Point]]) -> frozenset[Point]:
+    overlaps: dict[int, list[tuple[int, Callable[[Point], Point]]]] = defaultdict(list)
+    for i, j in ((i, j) for i in range(len(reports)) for j in range(len(reports)) if i != j):
+        f = syncf(reports[i], reports[j])
+        if f is not None:
+            overlaps[i].append((j, f))
+    visited: set[int] = set()
+    def collect(i: int) -> frozenset[Point]:
+        visited.add(i)
+        return frozenset(reports[i]) | frozenset(chain.from_iterable(
+            map(f, collect(j))
+            for j, f in overlaps[i]
+            if j not in visited
+        ))
+    return collect(0)
+
+reports: Sequence[Sequence[Point]] = tuple(
+    tuple(
+        Point(*map(int, line.split(",")))
+        for line in report.strip().split("\n")[1:]
+    )
+    for report in raw.split("\n\n")
+)
+print(len(synchronize(reports)))
