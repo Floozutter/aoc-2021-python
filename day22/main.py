2d6928176e114ee91f617becf3ba5ffce05942cb
Floozutter <me@floozutter.site>
2021-12-21 22:19:44 -0800

    add skeleton for day22 part 2

--- day22/main.py
+++ day22/main.py
@@ -4,30 +4,31 @@ with open(INPUTPATH) as ifile:
     raw = ifile.read()
 
 import re
-from typing import NamedTuple, Sequence
+from typing import NamedTuple, Iterable
+
+class Cuboid(NamedTuple):
+    x_min: int; x_max: int; y_min: int; y_max: int; z_min: int; z_max: int
+    def volume(self) -> int:
+        return (self.x_max+1-self.x_min) * (self.y_max+1-self.y_min) * (self.z_max+1-self.z_min)
 
 int_pattern = re.compile("-?[0-9]+")
 class Instruction(NamedTuple):
-    on: bool; x_min: int; x_max: int; y_min: int; y_max: int; z_min: int; z_max: int
+    on: bool
+    cuboid: Cuboid
     @classmethod
     def from_line(cls, line: str):
         l, r = line.strip().split()
-        return cls(l == "on", *map(int, int_pattern.findall(r)))
+        return cls(l == "on", Cuboid(*map(int, int_pattern.findall(r))))
 
-def init(instructions: Sequence[Instruction]) -> frozenset[tuple[int, int, int]]:
-    cubes: frozenset[tuple[int, int, int]] = frozenset()
-    for i in instructions:
-        region = {
-            (x, y, z)
-            for x in range(i.x_min, i.x_max+1)
-            for y in range(i.y_min, i.y_max+1)
-            for z in range(i.z_min, i.z_max+1)
-        }
-        if i.on:
-            cubes |= region
-        else:
-            cubes -= region
-    return cubes & {(x,y,z) for x in range(-50,51) for y in range(-50,51) for z in range(-50,51)}
+def update(state: tuple[Cuboid, ...], instructions: Iterable[Instruction]) -> tuple[Cuboid, ...]:
+    return ()
 
 instructions = tuple(map(Instruction.from_line, raw.strip().split("\n")))
-print(len(init(instructions[:20])))
+inits = next(j for j, inst in enumerate(instructions) if any(abs(z) > 50 for z in inst.cuboid))
+reactor: tuple[Cuboid, ...] = ()
+
+for inst in instructions[:inits]: reactor = update(reactor, inst)
+print(sum(cuboid.volume() for cuboid in reactor))
+
+for inst in instructions[inits:]: reactor = update(reactor, inst)
+print(sum(cuboid.volume() for cuboid in reactor))
